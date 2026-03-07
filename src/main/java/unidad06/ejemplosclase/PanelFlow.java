88b2697aa16f5bb03fba12e874a10a1fae489279
dgs <dgaysae524@g.educaand.es>
2026-03-07 19:41:35 +0100

    Arreglados algunos códigos ausentes.

--- src/main/java/unidad06/ejemplosclase/PanelFlow.java
+++ src/main/java/unidad06/ejemplosclase/PanelFlow.java
@@ -10,7 +10,13 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 /**
- *
+ * Este panel personalizado dibuja una etiqueta y un campo de texto, permitiendo:
+ * <ul>
+ *     <li>Indicar el texto de la etiqueta.</li>
+ *     <li>Establecer el ancho del campo de texto.</li>
+ *     <li>Ponerle un color de fondo al panel.</li>
+ *     <li>Indicar el título del borde del panel.</li>
+ * </ul>
  * @author diego
  */
 public class PanelFlow extends JPanel {
--- src/main/java/unidad06/ejemplosclase/VentanaBoxLayout.java
+++ src/main/java/unidad06/ejemplosclase/VentanaBoxLayout.java
@@ -8,6 +8,8 @@ import javax.swing.JFrame;
 
 /**
  *
+ * @see PanelFlow
+ * @see PanelVertical
  * @author diego
  */
 public class VentanaBoxLayout extends JFrame {
