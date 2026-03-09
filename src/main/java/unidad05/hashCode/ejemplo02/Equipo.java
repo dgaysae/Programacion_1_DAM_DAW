34e473294fd866614d6a22caaefcf462c8e5fe49
dgs <dgaysae524@g.educaand.es>
2026-03-09 17:02:01 +0100

    Arreglados algunos códigos ausentes.

--- src/main/java/unidad05/hashCode/ejemplo02/Equipo.java
+++ src/main/java/unidad05/hashCode/ejemplo02/Equipo.java
@@ -6,19 +6,21 @@ import java.util.Objects;
  * Ejemplo extraído de: https://www.baeldung.com/java-equals-hashcode-contracts
  *
  * El método .hashCode() se utiliza para comparar objetos en estructuras de datos
- * que utilicen tablas hash.
+ * que utilicen tablas hash (HashSet, HashMap, etc.).
  * 
  * Este método devuelve un entero que representa la instancia actual
  * de la clase. Debemos calcular este valor de manera coherente con la
- * definición del método equals().
+ * definición del método equals(). Es decir, que si equals evalúa unos campos para
+ * validar la igualdad con otros objetos, hashCode debe usar los mismos campos para
+ * calcular el codigo hash.
  *
  * Java SE define un contrato para el método .hashCode():
- * 1. Consistencia interna: el valor de hashCode() solo puede cambiar si cambia
- * una propiedad que está en equals()
- * 2. Consistencia de igualdad: los objetos que son iguales entre sí deben
- * devolver el mismo código hash.
- * 3. colisiones: objetos desiguales pueden tener el mismo código hash.
- *
+ * <ol>
+ *     <li>Consistencia interna: el valor de hashCode() solo puede cambiar si cambia una propiedad que está en equals()</li>
+ *     <li>Consistencia de igualdad: los objetos que son iguales entre sí deben devolver el mismo código hash.</li>
+ *     <li>Colisiones: objetos desiguales pueden tener el mismo código hash.</li>
+ * </ol>
+ * En los casos donde dos objetos distintos puedan tener el mismo hash, se recurre al método equals para desempatar.
  *
  * @author diego
  */
